caf03635e87207e6d97e66b3ef4d7b30385875fc
mehrdadsamadi <samadimehrdad49@gmail.com>
2024-07-13 15:17:46 +0330

    clean some files

--- components/FileUploader.tsx
+++ components/FileUploader.tsx
@@ -2,7 +2,7 @@
 
 import { convertFileToUrl } from '@/lib/utils'
 import Image from 'next/image'
-import React, { useCallback } from 'react'
+import { useCallback } from 'react'
 import { useDropzone } from 'react-dropzone'
 
 type FileUploaderProps = {
@@ -14,7 +14,7 @@ const FileUploader = ({ files, onChange }: FileUploaderProps) => {
     const onDrop = useCallback((acceptedFiles: File[]) => {
         onChange(acceptedFiles)
     }, [])
-    const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
+    const { getRootProps, getInputProps } = useDropzone({ onDrop })
 
     return (
         <div {...getRootProps()} className='file-upload'>
